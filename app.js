cc57fef454db63bdf6f12d3ab9e21f71accb470c
Mohammed Nazar <x3raqe@gmail.com>
2024-08-04 23:09:35 +0300

    done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
 
-// Routes 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
-
+// Routes
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // 1 Middlewares
 if (process.env.NODE_ENV === 'development') {
@@ -24,11 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 // 4. START THE SERVER
-const dotenv = require('dotenv').config({ path: './config.env' });
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
